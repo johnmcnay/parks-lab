9b62a2713a39be034dce05aacca4d9c4b4852cab
John McNay <John.McNay@improving.com>
2021-06-23 14:42:48 -0500

    adding caching

--- ParksLab/Controllers/HomeController.cs
+++ ParksLab/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using ParksLab.Models;
@@ -14,10 +15,12 @@ namespace ParksLab.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IMemoryCache _cache;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMemoryCache cache)
         {
             _logger = logger;
+            _cache = cache;
         }
 
         public IActionResult Index()
@@ -34,7 +37,19 @@ namespace ParksLab.Controllers
         public IActionResult Parks()
         {
 
-            var json = new WebClient().DownloadString("https://seriouslyfundata.azurewebsites.net/api/parks");
+            string json;
+
+            //from code sample in https://docs.microsoft.com/en-us/aspnet/core/performance/caching/memory?view=aspnetcore-5.0
+            //Look for cache key.
+            if (!_cache.TryGetValue(CacheKeys.Entry, out json))
+            {
+                // Key not in cache, so get data.
+                json = new WebClient().DownloadString("https://seriouslyfundata.azurewebsites.net/api/parks");
+
+                // Save data in cache and set the relative expiration time
+                _cache.Set(CacheKeys.Entry, json, TimeSpan.FromMinutes(5));
+            }
+
             List<ParkData> data = JsonConvert.DeserializeObject<List<ParkData>>(json);
             List<ParkData> results = new List<ParkData>();
 
@@ -64,8 +79,6 @@ namespace ParksLab.Controllers
                         results.Add(park);
                     }
                 }
-
-
 
                 ViewBag.Data = results;
             }
