3e47595a320a5e17bbad5aa07290f961d3de427d
John McNay <John.McNay@improving.com>
2021-06-23 08:05:15 -0500

    initial commit

---
+++ ParksLab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ParksLab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
